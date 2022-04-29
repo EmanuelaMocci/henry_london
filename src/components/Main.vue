ade01caa8fafb65a9ec67b0337100dc0c8b4ffe4
EmanuelaMocci <emanuelamocci2@gmail.com>
2022-04-29 15:28:50 +0200

    Added: New Collection Section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="main">
     <SectionHistory />
+    <SectionTemporaryStore />
+    <SectionNewCollection />
   </div>
 </template>
 
 <script>
 import SectionHistory from "./SectionHistory.vue";
+import SectionTemporaryStore from "./SectionTemporaryStore.vue";
+import SectionNewCollection from "./SectionNewCollection.vue";
 
 export default {
   name: "Main",
   components: {
     SectionHistory,
+    SectionTemporaryStore,
+    SectionNewCollection,
   },
 };
 </script>
---
+++ src/components/SectionNewCollection.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-lg-6 d-flex justify-content-end image">
+        <img src="../assets/img/collection.jpg" alt="" />
+      </div>
+      <div class="col-xs-12 col-lg-6 rigth-text">
+        <h4>LONDON COLLECTION SEASON</h4>
+        <h2>
+          New Collection <br />
+          Henry London
+        </h2>
+        <p>
+          On the other hand, we denounce with righteous indignation and dislike
+          men who are so beguiled and demoralized by the charms of pleasure of
+          the moment so blinded by desire
+        </p>
+        <button>View</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionNewCollection",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/partials/variables.scss";
+
+@mixin centered {
+  max-width: 85%;
+  margin: auto;
+}
+@mixin collection-title {
+  font-family: "Montserrat-bold";
+  font-size: 13px;
+  letter-spacing: 2.5px;
+}
+@mixin description {
+  color: $basic-color-3;
+  font-family: "Merriweather";
+  letter-spacing: 0.5px;
+  font-size: 14px;
+  line-height: 30px;
+  width: 60%;
+  padding-top: 50px;
+}
+@mixin button {
+  border: 1px solid $bg-logo;
+  color: $bg-logo;
+  background-color: transparent;
+  font-family: $font1;
+  font-size: 18px;
+  padding: 15px 100px;
+  transition: 0.5s;
+  margin: 50px 0 30px 0;
+}
+@mixin button-hover {
+  background-color: $button-color;
+  border: 1px solid $button-color;
+  color: $basic-color-1;
+}
+@mixin centered-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.container-fluid {
+  text-align: center;
+  @include centered;
+
+  h4 {
+    @include collection-title;
+  }
+  h2 {
+    font-family: $font1;
+    padding-top: 20px;
+  }
+  p {
+    @include description;
+  }
+  button {
+    @include button;
+    &:hover {
+      @include button-hover;
+    }
+  }
+  img {
+    width: 100%;
+  }
+}
+.row > * {
+  padding: 0;
+}
+.rigth-text {
+  @include centered-text;
+  padding-top: 30px;
+}
+
+$breakpoint-ipad: 992px;
+
+@media screen and (max-width: $breakpoint-ipad) {
+  .rigth-text {
+    order: 1;
+  }
+  .image {
+    order: 2;
+  }
+}
+</style>
