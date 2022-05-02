250f11d8065af76ba0a6c1d9dfaf00ad3c307901
EmanuelaMocci <emanuelamocci2@gmail.com>
2022-05-02 12:20:37 +0200

    Added: loader for initial rendering

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
-    <Navbar />
-    <Hero />
-    <Main />
-    <Footer />
+    <circles-to-rhombuses-spinner
+      :animation-duration="1200"
+      :circles-num="3"
+      :circle-size="15"
+      color="#c6b5a1"
+      v-if="loading"
+    />
+    <template v-else>
+      <Navbar />
+      <Hero />
+      <Main />
+      <Footer />
+    </template>
   </div>
 </template>
 
@@ -12,6 +21,7 @@ import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import Main from "./components/Main.vue";
 import Footer from "./components/Footer.vue";
+import { CirclesToRhombusesSpinner } from "epic-spinners";
 
 export default {
   name: "App",
@@ -20,6 +30,16 @@ export default {
     Hero,
     Main,
     Footer,
+    CirclesToRhombusesSpinner,
+  },
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  async mounted() {
+    await new Promise((r) => setTimeout(r, 2000));
+    this.loading = false;
   },
 };
 </script>
@@ -42,5 +62,14 @@ export default {
   font-family: "Merriweather";
   src: local("Merriweathert"),
     url(./font/Merriweather-Regular.ttf) format("truetype");
+}
+
+.circles-to-rhombuses-spinner {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 99;
 }
 </style>
